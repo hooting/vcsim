f738dc36b5da2669f819caa8877e9dce8ab4320e
xiaoxing.ma@gmail.com <xiaoxing.ma@gmail.com@ce9a68d9-aca3-b501-4369-88f9cb7ec176>
2010-08-10 05:20:27 +0000

    Enhancement to support record/replay

--- VCSim/src/it/polimi/vcdu/model/Transaction.java
+++ VCSim/src/it/polimi/vcdu/model/Transaction.java
@@ -4,6 +4,9 @@
 package it.polimi.vcdu.model;
 
 import it.polimi.vcdu.sim.ControlParameters;
+import it.polimi.vcdu.sim.Simulator;
+import it.polimi.vcdu.sim.record.Recorder;
+import it.unipr.ce.dsg.deus.core.Engine;
 
 import java.util.ArrayList;
 
@@ -20,14 +23,55 @@ public class Transaction {
 	private ArrayList<String> ancestors;
 
 	public Transaction(Component host, ArrayList<String> ancestors) {
-		this.id=host.getId()+"_T"+host.getTx_counter();
-		host.incrementTx_counter();
+
+		if(Simulator.getDefaultSimulator().isReplaying()){
+			Recorder recorder = Simulator.getDefaultSimulator().getRecorder();
+			Transaction tx;
+			if(ancestors.isEmpty()){
+				String rid = recorder.getNextRootTransactionID(host.getId());
+				tx = recorder.getNextTransction(host, ancestors, rid);
+			}else{
+				tx = recorder.getNextTransction(host, ancestors, ancestors.get(0));
+			}
+			this.id = tx.id;
+			this.ancestors = tx.ancestors;
+			this.host = host;
+			this.iteration = tx.iteration;
+			
+		}else{
+			this.id=host.getId()+"_T"+host.getTx_counter();
+			host.incrementTx_counter();
+			this.ancestors = new ArrayList<String>(ancestors);
+			this.ancestors.add(this.id);
+			this.host=host;
+			float localProcessingTime= ControlParameters.getCurrentParameters().getLocalProcessingTime();
+			this.iteration=new Iteration(localProcessingTime,host);
+			
+			if(Simulator.getDefaultSimulator().isRecording()){
+				Recorder recorder = Simulator.getDefaultSimulator().getRecorder();
+				if(ancestors.isEmpty()){
+					recorder.recordRootTxInjectTime(host.getId(), this.id, Engine.getDefault().getVirtualTime());
+				};
+				recorder.recordTransaction(this.getRootId(), this);
+			}
+		}
+	}
+	
+
+	/**
+	 * for replay purpose 
+	 * @param id
+	 * @param host
+	 * @param ancestors
+	 */
+	public Transaction(String id, Component host, ArrayList<String> ancestors, Iteration iteration) {
+		this.id=id;
 		this.ancestors = new ArrayList<String>(ancestors);
 		this.ancestors.add(this.id);
 		this.host=host;
-		float localProcessingTime= ControlParameters.getCurrentParameters().getLocalProcessingTime();
-		this.iteration=new Iteration(localProcessingTime,host);
+		this.iteration=iteration;
 	}
+	
 	/**
 	 * @return the id
 	 */
